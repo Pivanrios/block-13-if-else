cb2376759da4b7299d0d1b3f358be755525163c3
Pedro Perez <pipr96@gmail.com>
2023-04-18 10:24:55 -0700

    explain how and why we use for loops

---
+++ for-loops.js
@@ -0,0 +1,14 @@
+//Hello I know a little of JS and I wanna explain how
+//for loops work and why Im using it in this assignment.
+//for loops let you iterate as many times as needed.
+//For the assignment we have a list of values that need to be evaluated.
+//we are gonna create an array with the values to evaluate.
+const valuesToEvaluate = [1 ,3 ,"3","Hello", false, null]
+        //index begins at 0  1   2...               ...n-1
+//we create a for loop that goes tru each element of the array 
+//we initialize with a for
+for (i = 0; i < valuesToEvaluate.length; i++){
+//index starts; while index is less than    ;add one to index
+//    at 0    the length of array keep going
+    console.log(i)//print index
+}
